b015f79c01ea84c6daefff3fff7e0f5fd2a006ad
Levshenkov <levshenkov.denys@gmail.com>
2020-06-04 14:48:25 +0300

    readme api added

--- client/components/repos.js
+++ client/components/repos.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Markdown from 'markdown-to-jsx'
+import { useParams } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { setReadme } from '../redux/reducers/repositories'
 
-const Repos = (props) => {
-  const { data } = props
-
+const Repos = () => {
+  const readme = useSelector((store) => store.repositories.readme)
+  const { username, repositoryname } = useParams()
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(setReadme(username, repositoryname))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [repositoryname])
   return (
     <div>
       <div className="flex items-center justify-between bg-gray-200 pl-3 pr-2 py-3 w-full rounded text-gray-600 font-bold cursor-pointer hover:bg-gray-300">
         README.md:
       </div>
       <Markdown id="description" className="mb3 pl-3">
-        {data}
+        {readme}
       </Markdown>
     </div>
   )
--- client/redux/reducers/repositories.js
+++ client/redux/reducers/repositories.js
@@ -1,11 +1,13 @@
-// import axios from 'axios'
+import axios from 'axios'
 
 const UPDATE_USER_NAME = 'UPDATE_USER_NAME'
 const SET_REPOSITORIES = 'SET_REPOSITORIES'
+const README = 'README'
 
 const initialState = {
   username: '',
-  list: []
+  list: [],
+  readme: ''
 }
 
 export default (state = initialState, action) => {
@@ -20,6 +22,11 @@ export default (state = initialState, action) => {
         ...state,
         list: action.list
       }
+    case README:
+      return {
+        ...state,
+        readme: action.readme
+      }
     default:
       return state
   }
@@ -32,11 +39,21 @@ export function updateUsername(username) {
 export function setRepositories(username) {
   // eslint-disable-next-line func-names
   return function(dispatch) {
-    fetch(`https://api.github.com/users/${username}/repos`)
-      .then((r) => r.json())
-      .then((data) => {
-        const list = data.map((repo) => repo.name)
-        dispatch({ type: SET_REPOSITORIES, list })
-      })
+    axios(`https://api.github.com/users/${username}/repos`).then((it) => {
+      const list = it.data.map((repo) => repo.name)
+      dispatch({ type: SET_REPOSITORIES, list })
+    })
+  }
+}
+
+export function setReadme(username, repositoryname) {
+  // eslint-disable-next-line func-names
+  return function(dispatch) {
+    axios(
+      `https://api.github.com/repos/${username}/${repositoryname}/contents/README.md?ref=master`
+    ).then((text) => {
+      const readme = atob(text.data.content)
+      dispatch({ type: README, readme })
+    })
   }
 }
